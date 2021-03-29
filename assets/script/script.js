de6898a89b24b066687f91bc5f347fa6f77b9f3e
Saurav Ahuja <sauravahuja0412@gmail.com>
2021-03-29 23:57:25 +0530

    Home Section Completed

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#typer", {
+    strings: ['Passionate Engineer', 'Web Developer', 'Web Designer', 'UI/UX Designer',
+        'Graphic Designer', 'Full Stack Dev'
+    ],
+    typeSpeed: 120,
+    backSpeed: 40,
+    backDelay: 500,
+    startDelay: 500,
+    smartBackspace: true,
+    loop: true
+});
