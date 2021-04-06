cc3bc38ed63b8d4484748bf63a10201106c05113
Saurav Ahuja <sauravahuja0412@gmail.com>
2021-04-06 23:05:34 +0530

    Testimonial Section Completed

--- assets/script/script.js
+++ assets/script/script.js
@@ -25,3 +25,37 @@ $('.soft-skills').tilt({
     maxGlare: .6,
     // axis: 'x'
 })
+
+$('#review-slick').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    edgeFriction: 0.50,
+    prevArrow: "<button type='button' class='slick-prev'><i class=\"fa fa-angle-left\" aria-hidden=\"true\"></i></button>",
+    nextArrow: "<button type='button' class='slick-next'><i class=\"fa fa-angle-right\" aria-hidden=\"true\"></i></button>",
+    accessibility: false,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            // 600 or less than 600
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+})
