9aaa193dc0c5f3337a576e7069e25bc22542fc6b
Saurav Ahuja <sauravahuja0412@gmail.com>
2021-04-10 19:36:13 +0530

    Mobile Responsive Updates

--- assets/script/script.js
+++ assets/script/script.js
@@ -37,25 +37,15 @@ $('#review-slick').slick({
     prevArrow: "<button type='button' class='slick-prev'><i class=\"fa fa-angle-left\" aria-hidden=\"true\"></i></button>",
     nextArrow: "<button type='button' class='slick-next'><i class=\"fa fa-angle-right\" aria-hidden=\"true\"></i></button>",
     accessibility: false,
-    responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
-        },
+    responsive: [
         {
-            breakpoint: 600,
+            breakpoint: 500,
             // 600 or less than 600
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
             }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
     ]
 })
