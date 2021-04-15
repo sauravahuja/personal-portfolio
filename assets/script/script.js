85405e5d18744b9df9b4a94e887ec3452d01f737
Saurav Ahuja <sauravahuja0412@gmail.com>
2021-04-16 00:04:24 +0530

    Navbar Added With Responsiveness

--- assets/script/script.js
+++ assets/script/script.js
@@ -37,15 +37,95 @@ $('#review-slick').slick({
     prevArrow: "<button type='button' class='slick-prev'><i class=\"fa fa-angle-left\" aria-hidden=\"true\"></i></button>",
     nextArrow: "<button type='button' class='slick-next'><i class=\"fa fa-angle-right\" aria-hidden=\"true\"></i></button>",
     accessibility: false,
-    responsive: [
-        {
-            breakpoint: 500,
-            // 600 or less than 600
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false,
-            }
+    responsive: [{
+        breakpoint: 500,
+        // 600 or less than 600
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
         }
-    ]
+    }]
 })
+
+/******************************************************************
+            NAVBAR JS
+******************************************************************/
+
+const navBtn = document.getElementById('nav-btn');
+const navPanel = document.getElementById('navigation-panel');
+var first = document.getElementById('first');
+var second = document.getElementById('second');
+var last = document.getElementById('last');
+
+navBtn.addEventListener("click", navbarclicked);
+
+function navbarclicked() {
+    console.log("clicked")
+    if (navBtn.classList.contains('opened')) {
+        console.log("we have closed it for you");
+        second.style.visibility = "visible";
+        navPanel.style.transition = "all .3s ease-in-out";
+        navPanel.style.height = "0";
+        navPanel.style.borderBottomLeftRadius = "50%";
+        navPanel.style.borderBottomRightRadius = "50%";
+
+        navBtn.classList.remove("opened");
+        navBtn.classList.add("closed");
+
+        first.style.transform = "unset"
+
+        last.style.transform = "unset"
+
+        first.style.transition = "all .3s ease-in-out";
+
+        last.style.transition = "all .3s ease-in-out";
+    } else if (navBtn.classList.contains('closed')) {
+        console.log("we have opened it for you");
+        second.style.visibility = "hidden";
+        navPanel.style.transition = "all .3s ease-in-out";
+        navPanel.style.height = "100vh";
+        navPanel.style.position = "fixed";
+        navPanel.style.top = "0";
+        navPanel.style.bottom = "0";
+        navPanel.style.left = "0";
+        navPanel.style.right = "0";
+        navPanel.style.zIndex = "999";
+
+        navPanel.style.borderBottomLeftRadius = "0px";
+        navPanel.style.borderBottomRightRadius = "0px";
+
+        navBtn.classList.remove("closed");
+        navBtn.classList.add("opened");
+
+        first.style.transform = "rotate(45deg) translate(10px,10px)"
+
+        last.style.transform = "rotate(-45deg) translate(7px,-8px)"
+
+        first.style.transition = "all .3s ease-in-out";
+
+        last.style.transition = "all .3s ease-in-out";
+    }
+}
+
+function closeNav(){
+    if (navBtn.classList.contains('opened')) {
+        console.log("we have closed it for you");
+        second.style.visibility = "visible";
+        navPanel.style.transition = "all .3s ease-in-out";
+        navPanel.style.height = "0";
+        navPanel.style.borderBottomLeftRadius = "50%";
+        navPanel.style.borderBottomRightRadius = "50%";
+
+        navBtn.classList.remove("opened");
+        navBtn.classList.add("closed");
+
+        first.style.transform = "unset"
+
+        last.style.transform = "unset"
+
+        first.style.transition = "all .3s ease-in-out";
+
+        last.style.transition = "all .3s ease-in-out";
+    }
+}
